719ebaa5140d203455a510de8d3a301209878dfa
Jarred Witt <me@jarredwitt.com>
2016-03-31 10:02:44 -0500

    Added new navigation methods.

--- example/home.js
+++ example/home.js
@@ -2,16 +2,16 @@ import React, { PropTypes, Text, TouchableOpacity, View } from 'react-native';
 
 const propTypes = {
   data: PropTypes.any,
-  navigate: PropTypes.func,
+  navigate: PropTypes.object,
 };
 
 let Home = (props) => {
-  let modalNavigate = () => props.navigate({ type: 'push', key: 'page', title: 'Pushed from home tab', data: 'Some data from the home tab' });
+  let push = () => props.navigate.push('page', { title: 'Pushed from home tab', data: 'Some data from the home tab' });
 
   return (
     <View style={{ flex: 1, backgroundColor: 'rgba(0, 0, 0, 0.2)', alignItems: 'center', justifyContent: 'center' }}>
       <Text style={{ marginBottom: 10 }}>Home Page</Text>
-      <TouchableOpacity style={{ marginBottom: 10 }} onPress={modalNavigate}>
+      <TouchableOpacity style={{ marginBottom: 10 }} onPress={push}>
         <View style={{ backgroundColor: 'rgba(0, 0, 255, 0.6)' }}>
           <Text style={{ padding: 20 }}>Push Page</Text>
         </View>
