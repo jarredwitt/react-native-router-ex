76b05e1e48ba26bd8edba3691ac03b56ff299c95
Jarred Witt <me@jarredwitt.com>
2016-05-10 09:31:08 -0500

    Cleaned up some code. Created constants for nav actions. Added modalPop navigation method.

--- example/page.js
+++ example/page.js
@@ -6,22 +6,32 @@ const propTypes = {
   data: PropTypes.any,
   navigate: PropTypes.object,
   parent: PropTypes.string,
+  modal: PropTypes.bool,
   dispatch: PropTypes.func,
 };
 
 let Page = (props) => {
   let push = () => props.dispatch(props.navigate.push('nested', { title: 'Pushed from within page', data: 'Some data from the pushed page tab', parent: props.parent }));
+  let closeModal = () => props.dispatch(props.navigate.modalPop());
 
   return (
     <View style={{ flex: 1 }}>
       <ScrollView style={{ flex: 1, backgroundColor: 'rgba(0, 0, 0, 0.8)' }} contentContainerStyle={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
         <Text style={{ marginBottom: 10 }}>A pushed page</Text>
         <Text style={{ marginBottom: 10 }}>{props.data}</Text>
-        <TouchableOpacity style={{ marginBottom: 10 }} onPress={push}>
-          <View style={{ backgroundColor: 'rgba(0, 0, 255, 0.6)' }}>
-            <Text style={{ padding: 20 }}>Push Page</Text>
-          </View>
-        </TouchableOpacity>
+        {props.modal ?
+          <TouchableOpacity style={{ marginBottom: 10 }} onPress={closeModal}>
+            <View style={{ backgroundColor: 'rgba(0, 0, 255, 0.6)' }}>
+              <Text style={{ padding: 20 }}>Close Modal</Text>
+            </View>
+          </TouchableOpacity>
+          :
+          <TouchableOpacity style={{ marginBottom: 10 }} onPress={push}>
+            <View style={{ backgroundColor: 'rgba(0, 0, 255, 0.6)' }}>
+              <Text style={{ padding: 20 }}>Push Page</Text>
+            </View>
+          </TouchableOpacity>
+        }
       </ScrollView>
     </View>
   );
